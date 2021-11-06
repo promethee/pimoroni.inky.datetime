0674c237fe921a5b77a8833f0689ec8ea42fbd8a
promethee <lunette.francois@gmail.com>
2021-11-06 23:55:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import time
 from inky.auto import auto
+from datetime import date
+from datetime import datetime
+from pytz import timezone
+import pytz
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 from fonts.ttf import RobotoMedium
 
@@ -27,9 +32,37 @@ def draw_credits(text, h):
   x,y = 16, (h - int(text_h * 0.25))
   draw.text((x, y), text, font=font_smiley, fill=BLACK)
 
+def show_time():
+  draw.rectangle((0, 0, WIDTH, HEIGHT), fill=WHITE)
+  now = datetime.now().astimezone(None)
+  texts = [now.strftime("%b %d"), now.strftime("%H:%M")]
+  margin = 4
+  vertical_offsets = [margin / 4, HEIGHT / 2 + margin / 4]
+  index = 0
+
+  for text in texts:
+    fontsize = 100
+    font = ImageFont.truetype(RobotoMedium, fontsize)
+    size = draw.textsize(text, font)
+    target_size = [WIDTH - margin, HEIGHT / 2 - margin]
+    while size[0] > WIDTH or size[1] > target_size[1]:
+      fontsize = fontsize - 2
+      font = ImageFont.truetype(RobotoMedium, fontsize)
+      size = draw.textsize(text, font)
+    print("using fontsize:", fontsize)
+    horizontal_offset = (WIDTH / 2) - (size[0] / 2) - (margin / 2)
+    draw.text((horizontal_offset, vertical_offsets[index]), text, BLACK, font)
+    index += 1
+  inky_display.set_image(img)
+  inky_display.show(img)
+
 draw_credits("¯\_(ツ)_/¯", HEIGHT*0.05)
 draw_credits("promethee", HEIGHT*0.35)
 draw_credits("@github", HEIGHT*0.65)
 inky_display.set_image(img)
 inky_display.show(img)
+time.sleep(3)
 
+while True:
+  show_time()
+  time.sleep(60)
